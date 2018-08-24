90fa8e6d1e783de2c2ebb1534dc71ccd09a363ee
Madeleine Mackey <mackeymadeleine@gmail.com>
2018-08-24 15:01:09 +1000

    Stuff to get local server working

--- index.js
+++ index.js
@@ -57,15 +57,14 @@ function queryDatabase()
    
 //creates server on website
 var server = http.createServer(function(request, response) {
-	fs.readFile('home.html', function(err, data) {
+	fs.readFile('index.html', function(err, data) {
 		response.writeHead(200, {'Content-Type': 'text/html'});
 		response.write(data);
-		response.end();
 	});
 });
 
 var port = process.env.PORT || 1337;
-server.listen(port);
+server.listen(port, 'localhost');
 
 console.log("Server running at http://localhost:%d", port);
 
