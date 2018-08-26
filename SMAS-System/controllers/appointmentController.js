87b22f18d87a6bef53f7713576d5381aab803343
AJUnsworth <33707724+AJUnsworth@users.noreply.github.com>
2018-08-26 17:19:02 +1000

    U201: Create Appointment - Controller + View
    Implemented controller and view for create appointments, added validation checks

--- SMAS-System/controllers/appointmentController.js
+++ SMAS-System/controllers/appointmentController.js
@@ -1,4 +1,7 @@
-//var Appointment = require('../models/appointment')
+var Appointment = require('../models/appointment')
+// Validation of form data
+const {body,validationResult} = require('express-validator/check')
+const {sanitizeBody} = require('express-validator/filter')
 
 // Display home page
 exports.index = function(req, res) {
@@ -7,13 +10,34 @@ exports.index = function(req, res) {
 
 // Display Appointment creation form on GET
 exports.appointment_create_get = function(req, res){
-    res.send('To be implemented');
+    res.render('createAppointment', { title: 'Create an Appointment' });
 };
 
 // Handle Appointment creation form on POST
-exports.appointment_create_post = function(req, res){
-    res.send('To be implemented')
-};
+exports.appointment_create_post = [
+     //Field Validation
+    body('student_id').isLength({ min: 8, max: 8 }).trim().withMessage('Enter valid Student ID'),
+    body('description').isLength({ max: 200 }).trim().withMessage('Description must be specified'),
+    body('time').isISO8601(),
+
+        // Field sanitisation
+    sanitizeBody('student_id').trim().escape(),
+    sanitizeBody('description').trim().escape(),
+    sanitizeBody('time').toDate(),
+
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if(!errors.isEmpty()) {
+                res.render('createAppointment', { title: 'Create an Appointment', errors: errors.array() });
+                return;
+            }
+            else {
+                res.redirect('/');
+            }
+        }
+
+
+];
 
 // Display Appointment deletion form on GET
 exports.appointment_delete_get = function(req, res){
