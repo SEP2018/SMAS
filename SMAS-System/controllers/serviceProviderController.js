a1b261250d9d327a5a56e6f7fa860bb88c473f68
James MacNaughton <ziga328@gmail.com>
2018-09-02 00:55:35 +1000

    deleted serviceProviderController

--- SMAS-System/controllers/serviceProviderController.js
+++
@@ -1,7 +0,0 @@
-var Appointment = require('../models/serviceProvider');
-var staffForService = Appointment.getAllStaffForService(3);
-staffForService.then(async function(){
-    staffForService = await staffForService;
-    console.log("waiting");
-    console.log(staffForService);
-});
