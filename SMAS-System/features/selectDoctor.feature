1f25a300b0b287c54ebd6d00263b2a4ba551ff66
AndrewJUnsworth <33707724+AJUnsworth@users.noreply.github.com>
2018-09-14 18:32:01 +1000

    Added Cucumber functionality
    Included example test case to check if building works

---
+++ SMAS-System/features/selectDoctor.feature
@@ -0,0 +1,9 @@
+#SelectDoctor.feature
+  Feature: Select Preferred Doctor
+      I should be able to select a preferred doctor
+
+  Scenario: Doctor is selected
+    Given a user is logged in to SMAS
+    And they navigate to the Create Appointment screen
+    When they select a preferred doctor
+    Then the selected doctor is assigned to the appointment
---
+++ SMAS-System/features/step_definitions/stepdefs.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+    function checkDoctor(doctor) {
+        return 'Unsworth';
+    }
+
+    Given('a user is logged in to SMAS', function () {
+
+    });
+
+    Given('they navigate to the Create Appointment screen', function () {
+
+    });
+
+    When('they select a preferred doctor', function () {
+        this.actualAnswer = checkDoctor('Unsworth');
+    });
+
+    Then('the selected doctor is assigned to the appointment', function () {
+        assert.equal(this.actualAnswer, 'Unsworth');
+    });
