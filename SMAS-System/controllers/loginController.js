4ebe0c40bc608de923da021b83ebde18a7130105
AndrewJUnsworth <33707724+AJUnsworth@users.noreply.github.com>
2018-09-23 17:24:44 +1000

    Added route for staff daily and weekly pages
    To access use localhost:(PORT)/staff/daily or localhost:(PORT)/staff/weekly

--- SMAS-System/controllers/loginController.js
+++ SMAS-System/controllers/loginController.js
@@ -8,3 +8,7 @@ const {sanitizeBody} = require('express-validator/filter');
 exports.login_get = function(req, res){
     res.render('login', {title: 'login'});
 };
+
+exports.login_post = function(req, res){
+    res.render('login', {title: 'login'});
+};
---
+++ SMAS-System/routes/staff.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var staff_daily_controller = require('../controllers/staffDailyController');
+var staff_weekly_controller = require('../controllers/staffWeeklyController');
+
+// GET daily appointments
+router.get('/daily', staff_daily_controller.staff_daily_get);
+
+// POST daily appointments
+router.post('/daily', staff_daily_controller.staff_daily_post);
+
+// GET weekly appointments
+router.get('/weekly', staff_weekly_controller.staff_weekly_get);
+
+// POST weekly appointments
+router.post('/weekly', staff_weekly_controller.staff_weekly_post);
+
+module.exports = router;
