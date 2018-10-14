703259fee0e25a0642ae8eb151c95d920421bf15
AndrewJUnsworth <33707724+AJUnsworth@users.noreply.github.com>
2018-10-14 23:21:38 +1100

    Updated tests

---
+++ SMAS-System/features/step_definitions/calendarReminderStepdefs.js
@@ -0,0 +1,28 @@
+// const assert = require('assert');
+// const { Given, When, Then, defineSupportCode } = require('cucumber');
+// const seleniumWebDriver = require('selenium-webdriver');
+// const By = seleniumWebDriver.By;
+// const Until = seleniumWebDriver.until;
+// const chai = require('chai');
+// const chaiAsPromised = require('chai-as-promised');
+// chai.use(chaiAsPromised);
+// const expect = chai.expect;
+//
+//
+//     defineSupportCode(function ({When, Then, Given}) {
+//
+//         Given('a user has scheduled an appointment', function () {
+//             To be implemented with Calendar event
+//         });
+//
+//         When("the user opts to create a calendar event", function () {
+//              To be implemented with Calendar event
+//         });
+//
+//         When("the Google account authentication is successful", function() {
+//             To be implemented with Calendar event
+//         });
+//
+//         Then('a Google Calendar reminder is created for the appointment', function (callback) {
+//             To be implemented with Calender event
+//     });
